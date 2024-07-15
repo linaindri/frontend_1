e83a9c8426dc78ea562e19d3907aec1a47e73012
linaindrii <andriyanilina73@gmail.com>
2024-07-15 17:10:41 +0700

    Pesan commit Anda di sini

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Landing from './Pages/Landing';
 import State from './Pages/State';
-
+import EventHandling from './Pages/EventHandling'
 
 function App () {
   return (
@@ -12,6 +12,7 @@ function App () {
         <Route path="/" Component={Home}/>
         <Route path="/landingpage" Component={Landing}/>
         <Route path="/state" Component={State}/>
+        <Route path="/eventhandling" Component={EventHandling}/>
       </Routes>
     </>
   )
